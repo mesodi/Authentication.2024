e283fd63030085f65e820aa01b86324c4f7fdf77
Bassam <shahoudbassam@gmail.com>
2024-04-08 12:10:09 +0200

    commit

--- src/main/java/User/Interface/Implementation/Authentication/Controller/UserController.java
+++ src/main/java/User/Interface/Implementation/Authentication/Controller/UserController.java
@@ -1,4 +1,44 @@
 package User.Interface.Implementation.Authentication.Controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+    @GetMapping("/error")
+    public String handleError() {
+        // Logic to handle errors
+        return "error";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        model.addAttribute("username", userDetails.getUsername());
+        return "dashboard";
+    }
+
+
+    @PostMapping("/login")
+    public String loginPost() {
+        // Handle login logic if needed
+        System.out.println("Login successful");
+        return "redirect:/dashboard";
+    }
+
+
+    @PostMapping("/logout")
+    public String logout() {
+        // Perform logout logic if needed
+        return "redirect:/login?logout";
+    }
+
 }
